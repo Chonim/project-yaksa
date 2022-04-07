b062f44a3f938475dc7c3f0894df0ff094f62c19
Chiho Yoon <heisenberg.yoon@freedgrouptech.com>
2022-04-07 13:30:26 +0900

    feat: implement bug list

--- pages/bung/index.tsx
+++ pages/bung/index.tsx
@@ -1,37 +1,20 @@
-import { Grid, List, ListItem, ListItemText, styled, Typography } from '@mui/material'
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
 import React from 'react'
 
-const Demo = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-}));
-
-function generate(element: React.ReactElement) {
-  return [0, 1, 2, 3].map((value) =>
-    React.cloneElement(element, {
-      key: value,
-    }),
-  );
-}
-
 const BungListPage = () => (
-  <Grid container spacing={2}>
-    <Grid item xs={12} md={6}>
-      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-        Text only
-      </Typography>
-      <Demo>
-        <List dense={false}>
-          {generate(
-            <ListItem>
-              <ListItemText
-                primary="Single-line item"
-                secondary={false} />
-            </ListItem>
-          )}
-        </List>
-      </Demo>
-    </Grid>
-  </Grid>
+  <List dense={false}>
+    {[0, 1, 2, 3].map((value) => {
+      return (
+        <ListItem key={value}>
+          <ListItemButton>
+            <ListItemText
+              primary={`벙 ${value}`}
+              secondary={false} />
+          </ListItemButton>
+        </ListItem>
+      )
+    })}
+  </List>
 )
 
 export default BungListPage
